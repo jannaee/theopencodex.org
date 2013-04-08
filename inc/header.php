6c930397e21e60c913d19ecfb9532630347ede51
Nico Ekkart <nico_ekkart@outlook.com>
2013-04-08 23:25:04 +0300

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -31,15 +31,15 @@
 
 						<!-- Navigation Items -->
 						<ul class="nav">
-							<li><a href="index.php">Home</a></li>
+							<li><a href="/index.php">Home</a></li>
 							<li class="divider-vertical"></li>
-							<li><a href="projects/">Projects</a></li>
+							<li><a href="/projects/">Projects</a></li>
 							<li class="divider-vertical"></li>
-							<li><a href="downloads/">Downloads</a></li>
+							<li><a href="/downloads/">Downloads</a></li>
 							<li class="divider-vertical"></li>
-							<li><a href="links/">Links</a></li>
+							<li><a href="/links/">Links</a></li>
 							<li class="divider-vertical"></li>
-							<li><a href="about/">About</a></li>
+							<li><a href="/about/">About</a></li>
 						</ul>
 					</div>
 				</div>
